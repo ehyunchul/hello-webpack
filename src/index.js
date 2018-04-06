862a005fe2b357c8366ffc1a5effddec1a1d12c2
HYUNCHUL <chul0830@gmail.com>
2018-04-07 02:42:11 +0900

    Ouput Management 준비

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Ryan from './images/ryan.png';
-import Data from './data/data.xml';
 
 function component() {
     let element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    let ryanImage = new Image();
-    ryanImage.src = Ryan;
-
-    element.appendChild(ryanImage);
-
-    console.log(Data);
 
     return element;
 }
