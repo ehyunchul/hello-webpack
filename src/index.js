c2302d4fb082b8f875d6c9f1dbb1316f49df39b8
HYUNCHUL <chul0830@gmail.com>
2018-04-07 02:37:05 +0900

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import _ from 'lodash';
+import './style.css';
+import Ryan from './images/ryan.png';
+import Data from './data/data.xml';
 
 function component() {
-  var element = document.createElement('div');
+    let element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
-  return element;
+    let ryanImage = new Image();
+    ryanImage.src = Ryan;
+
+    element.appendChild(ryanImage);
+
+    console.log(Data);
+
+    return element;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,49 @@
 const path = require('path')
 
 const config = {
-	entry: {
-		main: './src/index.js'
-	},
-	output: {
-		filename: 'bundle.js',
-		path: __dirname + '/dist'
-	}
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname) + '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
+    }
 };
-
 
 
 module.exports = config;
