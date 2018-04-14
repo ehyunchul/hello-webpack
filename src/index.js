3519d925e0721785ef7dd1ff03a82a8df8628ca0
HYUNCHUL <chul0830@gmail.com>
2018-04-15 00:35:13 +0900

    Production

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import {cube} from './math.js';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+} else {
+    console.log('Looks like we are in production mode!');
+}
+
 function component() {
     let element = document.createElement('pre');
 
